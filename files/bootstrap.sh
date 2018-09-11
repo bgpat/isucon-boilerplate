51047a366e39af979abe09f5eea601eef8b55a84
Atsushi Tanaka <bgpat@users.noreply.github.com>
2018-09-12 00:13:01 +0900

    Update bootstrap.sh

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -25,3 +25,12 @@ EOF
 
 echo 'PermitUserEnvironment yes' >> /etc/ssh/sshd_config
 sshd -t && systemctl restart sshd
+
+echo -en "Please type github repository (e.g. \e[4mgit@github.com:bgpat/isucon.git\e[0m):\n> "
+read repo
+git remote set-url origin $repo
+
+yes | ssh-keygen -f /root/.ssh/id_rsa -t rsa -N "" > /dev/null
+repo=${repo##*:}
+echo -e "Open \e[4mhttps://github.com/${repo%.git}/settings/keys/new\e[0m and paste pubkey:\n"
+cat /root/.ssh/id_rsa.pub
